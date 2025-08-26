35440877f7b6eeb130c696c261a00422743c78b1
KSPravi <praviks.123@gmail.com>
2025-08-27 02:18:21 +0530

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,46 @@
+$(".service-carousel").owlCarousel({
+  loop: true,
+  margin: 30,
+  dots: false,
+  nav: true,
+  items: 3,
+  autoplay: true,
+  autoplayTimeout: 4000,
+  smartSpeed: 2500,
+  navText: [
+    "<img src='images/arrow.svg' alt='Prev'>",
+    "<img src='images/arrow.svg' alt='Next'>",
+  ],
+  responsive: {
+    0: {
+      items: 1,
+    },
+
+    480: {
+      items: 2,
+    },
+
+    768: {
+      items: 2,
+    },
+    992: {
+      items: 3,
+    },
+  },
+});
+
+$(".testimonial-carousel").owlCarousel({
+  loop: true,
+  margin: 30,
+  dots: true,
+  nav: false,
+  items: 1,
+  autoplay: true,
+  autoplayTimeout: 4500,
+  smartSpeed: 2500,
+});
+
+$(".toggle-menu").click(function () {
+  $(this).toggleClass("menu-active");
+  $(".main_menu").toggleClass("menu-opened");
+});
